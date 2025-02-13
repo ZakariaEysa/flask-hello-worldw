5d17ceaae3d66db61bea669d2912c10e3a6c5582
ZakariaEysa <124531014+ZakariaEysa@users.noreply.github.com>
2025-02-13 15:54:31 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
+
 from sklearn.metrics.pairwise import cosine_similarity
 
 
